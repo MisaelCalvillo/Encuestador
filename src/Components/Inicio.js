555a3edbe2ee0f1577d1138052a01433dfdf6071
Daniel Calvillo <danielcalvillomancilla123@gmail.com>
2020-06-13 22:28:08 -0500

    Redirect User To Encuesta

--- src/Components/Inicio.js
+++ src/Components/Inicio.js
@@ -7,7 +7,6 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
@@ -36,7 +35,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function Inicio({dispatch}) {
+function Inicio({dispatch, history}) {
   const classes = useStyles();
   const [emailValue, setEmailValue] = useState('')
   const [passwordValue, setPasswordValue] = useState('')
@@ -55,7 +54,7 @@ function Inicio({dispatch}) {
       .then(result => {
         if(result.user.email){
           console.log(result.user.email)
-
+          history.push(`/encuesta`)
         } else {
           alert('no existes :c')
           firebase.auth().signOut()
